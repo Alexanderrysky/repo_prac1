39f3771294f9b5661e933c036047ad2d3f15f225
Alexanderrysky <alexanderrysky@gmail.com>
2023-07-04 13:00:51 +0400

    vsql practise adding tables
    i added dimension and fact tables with their schemas

--- SQL practise.py
+++ SQL practise.py
@@ -1,1 +1,22 @@
-print("hello")
+creat table if not exists data_exploratin.dim_games
+(
+    id_game              Integer,
+    game_name_en         string,
+    publisher_name_en    string,
+    release_date         date
+)
+
+creat table if not exists data_exploration.dim_genre
+(
+    id_genre        integer,
+    genre_name_en   string
+)
+
+creat table if not exists data_exploration.fact_ratings
+(
+    fk_game         integer,
+    game_name_en    string,
+    fk_genre        integer,
+    genre_name_en   string,
+    ratings_value   float
+)
