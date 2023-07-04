5c299ad74b2f1dcfdabc551e1db228f370ecf473
Alexanderrysky <alexanderrysky@gmail.com>
2023-07-04 16:12:05 +0400

    updated mistakes
    fixed forgotten commas and un-needed ""

--- SQL_alterations.sql
+++ SQL_alterations.sql
@@ -2,6 +2,7 @@ ALTER TABLE data_exploration.dim_games
 ADD COLUMN maximum_score;
 
 UPDATE data_exploration.fact_ratings
-SET 'score' = 10;
+SET maximum_score = 10
+WHERE 1=1;
 
-DROP table data_exploration.fact_ratings;
+DROP table data_exploration.dim_games;
