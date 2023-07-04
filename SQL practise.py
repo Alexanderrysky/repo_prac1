ee88f524c39c3c81ee553bdc2e8c5539c00b7226
Alexanderrysky <alexanderrysky@gmail.com>
2023-07-04 13:17:21 +0400

    efficiany upgrades
    reduced integer to int and added more schemas in the fact_rating whilst getting rid of some

--- SQL practise.py
+++ SQL practise.py
@@ -1,12 +1,12 @@
 creat table if not exists data_exploratin.dim_games
 (
-    id_game              Integer,
+    id_game              int,
     game_name_en         string,
     publisher_name_en    string,
     release_date         date
 )
 
-creat table if not exists data_exploration.dim_genre
+creat table if not exists data_exploration.dim_genres
 (
     id_genre        integer,
     genre_name_en   string
@@ -14,9 +14,10 @@ creat table if not exists data_exploration.dim_genre
 
 creat table if not exists data_exploration.fact_ratings
 (
-    fk_game         integer,
-    game_name_en    string,
-    fk_genre        integer,
-    genre_name_en   string,
-    ratings_value   float
+    fk_game                int,
+    fk_genre               int,
+    ratings_value          float,
+    rating_validity_date   date,
+    audience_ratings       float,
+    audience_validity_date float,
 )
