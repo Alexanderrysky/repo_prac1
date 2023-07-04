7c1ab285b3cc4126e7b42487d8086adda0e90187
Alexanderrysky <alexanderrysky@gmail.com>
2023-07-04 16:58:24 +0400

    practicing selection and joining of tables sql

--- sql_analytics.sql
+++ sql_analytics.sql
@@ -1,1 +1,5 @@
-select
+select a.gamer_username_en, b.country_name_en, a.id_gamer
+from data_exploration.dim_gamers a
+left join data_exploration.dim_countries b
+on a.fk_country = b.id_country
+order by a.id_gamer;
