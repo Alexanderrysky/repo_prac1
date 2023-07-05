ce57d7a737987a103ad1803f6718cd073454321a
Alexanderrysky <alexanderrysky@gmail.com>
2023-07-05 13:22:19 +0400

    added new sql code
    practicing the sql with already existing tables

--- sql_analytics.sql
+++ sql_analytics.sql
@@ -20,3 +20,22 @@ on c.id_game = a.fk_game
 left join data_exploration.dim_genres d
 on d.id_genre = a.fk_genre;
 
+#practising the sql with already exsiting tables
+
+select
+date(a.order_date) sales_date,
+a.currency,
+b.spryker_order_state,
+#a.exchange_rate,
+sum(c.shipping_fee)     gross_shipping_cost,
+SUM(a.item_gross_price)  gross_revenue,
+a.city_name_en
+from analytics.fact_items a
+left join gcp_gs.map_order_item_status b
+on a.fk_sales_order_item_state = CAST(b.fk_sales_order_item_state as integer)
+   and b.business_type='b2c'
+left join analytics.fact_orders c
+on a.fk_sales_order = c.id_sales_order
+where a.order_date >= '2023-07-03'
+and cast(b.reporting_order_item_state as integer) >= 2
+GROUP BY 1,2,3,6;
