360efc19c24499e4555b4cb8ccb1b75162e009ba
Alexanderrysky <alexanderrysky@gmail.com>
2023-07-05 17:02:30 +0400

    making visualization code
    making code in python to visualize the data in the tables

---
+++ CSV_data_visualisation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#df = pd.read_csv("revenue_july.csv")
+#print(df.to_string())
+#print(pd.options.display.max_rows)
+#pd.options.display.max_rows = 90
+#print(df.to_string())
+#print(pd.options.display.max_rows)
+#print(df.head(4))
+#print(df.tail(3))
+
+revenue = pd.read_csv("revenue_july.csv")
+#print(revenue.head())
+#revenue.plot.scatter(x = 'sales_date', y = 'gross_revenue')
+#plt.show()
+revenue['gross_revenue'] = revenue['gross_revenue'].astype(float)
+print(revenue.dtypes)
+plt.pie(revenue["gross_revenue"], labels = revenue["city_name_en"])
+plt.show()
